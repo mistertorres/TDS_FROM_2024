9b74f43c0611fd3a4ec75a48346bc5ba29adbb44
Claudemir Torres <mistertorres2017@gmail.com>
2024-08-12 21:35:57 -0300

    prova

---
+++ Prova/script.js
@@ -0,0 +1,17 @@
+const Nome = document.getElementById("Nome");
+const Email = document.getElementById("Email");
+
+function nome(nome){
+
+    if (mensagem_confirmação.innerHTML == "mensagem_confirmação"){
+    console.log("mensagem_confirmação ", "Muito obrigado(a), senhor(a)" );
+    console.log("Acabamos de enviar um e-mail para:")
+    return true;
+    }
+
+} 
+
+function botão(botão)
+{
+    onclick=botão;
+}
